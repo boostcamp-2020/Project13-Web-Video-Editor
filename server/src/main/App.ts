7676334cb4bc75ba939e0a697c2d9983e56f2e6d
mjseok <mjseok98@gmail.com>
2020-11-19 17:55:39 +0900

    [BE]: [FEAT] App express초기 설정
    express초기 설정

--- server/src/main/App.ts
+++ server/src/main/App.ts
@@ -1,40 +1,12 @@
 import express from 'express';
-import cors from 'cors';
-import indexRouter from '@/main/routes';
+import loader from '@/main/loaders';
 
-class App {
-  public app: express.Application;
+const app: express.Application = express();
 
-  /**
-   * @ class App
-   * @ method bootstrap
-   * @ static
-   *
-   */
-  public static bootstrap(): App {
-    return new App();
-  }
+const startServer = () => {
+  loader.init(app);
+};
 
-  constructor() {
-    this.app = express();
-    this.app.use(
-      cors({
-        origin: true,
-        credentials: true,
-      })
-    );
-    this.app.use('/', indexRouter);
-    this.app.get(
-      '/',
-      (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        res.send('Hello world');
-      }
-    );
-  }
-}
+startServer();
 
-export default App;
+export default app;
