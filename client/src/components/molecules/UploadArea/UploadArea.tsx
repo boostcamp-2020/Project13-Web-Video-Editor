22d89d5618db9614ea45c940db524eb3e63ba963
mjseok <mjseok98@gmail.com>
2020-11-20 10:52:34 +0900

    [FE] : [FEAT] #16 파일불러오기
    불러오기버튼을 누르면  내 컴퓨터를 선택해서 동영상을 불러올 수 있다
    서버에서 불러오는거 추후 구현

---
+++ client/src/components/molecules/UploadArea/UploadArea.tsx
@@ -0,0 +1,61 @@
+import React, { useState, createRef } from 'react';
+import { connect } from 'react-redux';
+import Button from '@/components/atoms/Button';
+import FileInput from '@/components/atoms/FileInput';
+import { FileInfo, startUpload, load } from '@/store/originalVideo/actions';
+
+interface Props {
+  startUpload: Function;
+  load: Function;
+}
+
+const initialFile = { name: undefined };
+
+const $video = document.createElement('video');
+
+const UploadArea: React.FC<Props> = ({ startUpload, load }) => {
+  const [visible, setVisible] = useState(false);
+  const [file, setFile] = useState(initialFile);
+  const ref: React.RefObject<HTMLInputElement> = createRef();
+
+  const handleChange = () => {
+    const localFile: File = ref.current?.files[0];
+    if (localFile) {
+      setFile(localFile);
+      startUpload();
+
+      const fileInfo: FileInfo = {
+        name: localFile.name,
+        extension: localFile.type.split('/')[1],
+        length: 0,
+      };
+
+      $video.ondurationchange = () => {
+        URL.revokeObjectURL($video.src);
+        fileInfo.length = $video.duration;
+
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+          load(reader.result, fileInfo);
+        });
+        reader.readAsArrayBuffer(localFile);
+      };
+      $video.src = URL.createObjectURL(localFile);
+    } else setFile(initialFile);
+    if (visible) setVisible(false);
+  };
+
+  return (
+    <>
+      <p>{file.name}</p>
+      <Button
+        message="불러오기"
+        onClick={() => setVisible(!visible)}
+        type="default"
+      />
+      {visible && <FileInput ref={ref} handleChange={handleChange} />}
+    </>
+  );
+};
+
+export default connect(null, { startUpload, load })(UploadArea);
