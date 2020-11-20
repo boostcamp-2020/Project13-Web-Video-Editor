e87e9cde8ceeae38f4d10ce9064c5398b39fb9d4
mjseok <mjseok98@gmail.com>
2020-11-20 10:25:12 +0900

    [FE] : [FEAT] #15 Canvas skeleton code
    Canvas를 구현하기 위한 틀 구성

---
+++ client/src/components/atoms/Canvas/Canvas.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCanvas = styled.canvas``;
+
+const Canvas: React.FC = () => <StyledCanvas />;
+
+export default Canvas;
