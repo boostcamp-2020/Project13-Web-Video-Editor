34d99337901d169457ff3801e7fb9d665c1f7d3d
mjseok <mjseok98@gmail.com>
2020-11-24 15:30:06 +0900

    [FE] : [FEAT] #24 video class getter 추가
    getVideo추가, getVideoWidth,getVideoHeight추가

--- client/src/video/video.tsx
+++ client/src/video/video.tsx
@@ -3,18 +3,33 @@ class Video {
 
   constructor() {
     this._video = document.createElement('video');
+    this._video.preload = 'auto';
   }
 
+  // getter
+  getVideo = () => {
+    return this._video;
+  };
+
+  getDuration = () => {
+    return this._video.duration;
+  };
+
+  getVideoWidth = () => {
+    return this._video.videoWidth;
+  };
+
+  getVideoHeight = () => {
+    return this._video.videoHeight;
+  };
+
+  // setter
   setSrc = (src: string) => {
     this._video.src = src;
   };
 
   setCurrentTime = (time: number) => {
     this._video.currentTime = time;
-  };
-
-  getDuration = () => {
-    return this._video.duration;
   };
 
   play = () => {
