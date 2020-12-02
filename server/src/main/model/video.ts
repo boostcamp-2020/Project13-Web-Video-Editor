e24de34a429173ccd8e97b0e48468a101d725c73
SSH1997 <akasiassh@naver.com>
2020-12-02 16:05:09 +0900

    [BE] : [FEAT] #5 createAPI  구현 및 타입 정의
    - insertId type을 정의하기 위해 @types/mysql2 패키지 설치
    - @types/multer devDependencies로 변경

--- server/src/main/model/video.ts
+++ server/src/main/model/video.ts
@@ -1,9 +1,22 @@
+import { OkPacket } from 'mysql2';
 import connection from './config';
 import query from './query';
 
 export const retrieveByUser = async (id: number) => {
-  const videoList = await connection.query(query.video.retrieveByUser, [id]);
-  return videoList;
+  const videos = (await connection.query(query.video.retrieveByUser, [id]))[0];
+  return videos;
+};
+
+export const create = async (
+  editor: number,
+  name: string,
+  video: string,
+  audio: string
+) => {
+  const id = ((
+    await connection.query(query.video.create, [editor, name, video, audio])
+  )[0] as OkPacket).insertId;
+  return id;
 };
 
 export default retrieveByUser;
