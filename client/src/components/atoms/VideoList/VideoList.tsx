7ba19f08ce7e00e6b4f1f1ab9ace4523370cc53b
권영언 <kyu9341@naver.com>
2020-12-03 15:11:03 +0900

    [FE]: [FEAT] VideoItem 구현

--- client/src/components/atoms/VideoList/VideoList.tsx
+++ client/src/components/atoms/VideoList/VideoList.tsx
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { fetchListStart } from '@/store/video/actions';
 import { getVideos } from '@/store/selectors';
 import color from '@/theme/colors';
+import VideoItem from './VideoItem';
 
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border: 1px solid ${color.BORDER};
-  background-color: ${color.BLACK};
+  background-color: ${color.MODAL};
+  border-radius: 12px 12px 0 0;
 `;
 
-const VideoItem = styled.div`
+const Header = styled.div`
   display: flex;
-  justify-content: space-between;
-  border: 1px solid ${color.BORDER};
-  background-color: ${color.BLACK};
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid ${color.BORDER};
+  padding: 10px;
+`;
+
+const List = styled.div`
+  height: 50vh;
+  overflow-y: auto;
 `;
 
 const VideoList: React.FC = () => {
+  const [selected, setSelected] = useState(null);
   const videos = useSelector(getVideos);
+  const dispatch = useDispatch();
+
+  const handleCheck = video => setSelected(video);
+
+  useEffect(() => {
+    if (!videos) dispatch(fetchListStart());
+  }, [videos]);
 
   return (
     <StyledDiv>
-      {videos.map(video => (
-        <VideoItem />
-      ))}
+      <Header>Video List</Header>
+      <List>
+        {videos?.map(video => (
+          <VideoItem
+            key={video.name}
+            video={video}
+            handleCheck={handleCheck}
+            selected={selected}
+          />
+        ))}
+      </List>
     </StyledDiv>
   );
 };
