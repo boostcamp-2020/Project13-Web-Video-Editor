14a12eb8201b84e6d1ddde069182acccf826707c
SSH1997 <akasiassh@naver.com>
2020-12-03 10:29:56 +0900

    [FE] : [FEAT] 비동기 처리(영상 업로드, 인코딩) 로직 구현

--- client/src/store/sagas.ts
+++ client/src/store/sagas.ts
@@ -1,6 +1,11 @@
 import { all, call, takeEvery } from 'redux-saga/effects';
-import { watchSetVideo, deleteSrc } from '@/store/originalVideo/sagas';
-import CropThumbnail from '@/store/crop/sagas';
+import {
+  watchSetVideo,
+  watchEncodeStart,
+  watchUploadStart,
+  deleteSrc,
+} from '@/store/originalVideo/sagas';
+import watchCrop from '@/store/crop/sagas';
 import { RESET } from './actionTypes';
 
 function* watchReset() {
@@ -8,5 +13,11 @@ function* watchReset() {
 }
 
 export default function* rootSaga() {
-  yield all([watchSetVideo(), watchReset(), CropThumbnail()]);
+  yield all([
+    watchSetVideo(),
+    watchEncodeStart(),
+    watchUploadStart(),
+    watchReset(),
+    watchCrop(),
+  ]);
 }
