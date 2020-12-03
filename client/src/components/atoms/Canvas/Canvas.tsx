985d43af49c1a08288168ce73a26fa6b351bb7b5
권영언 <kyu9341@naver.com>
2020-12-03 14:59:27 +0900

    [FE]: [REFACTOR] 안쓰는 파일 제거 및 atom, molecule 리팩토링

--- client/src/components/atoms/Canvas/Canvas.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const StyledCanvas = styled.canvas``;
-
-const Canvas: React.FC = () => <StyledCanvas />;
-
-export default Canvas;
