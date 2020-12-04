0eaba4281654abf522ff84a71b04bfe7ace6f449
joh16 <math_98@naver.com>
2020-12-04 12:52:30 +0900

    [FE] : [FIX] import 경로 오류 수정

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -3,7 +3,7 @@ import handleError from '@/utils/error';
 
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://115.85.181.29'
+    ? 'http://115.85.181.29:3000'
     : 'http://127.0.0.1:3000';
 
 const getInstance = (isFileOperation: boolean) =>
