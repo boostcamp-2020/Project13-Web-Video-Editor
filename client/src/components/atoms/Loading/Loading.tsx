ac678c0dbbb7560a4b32b8fb0ef82936fe2e9598
mjseok <mjseok98@gmail.com>
2020-12-08 16:23:21 +0900

    [FE] : [FEAT] #42 Loading화면
    loading화면 변경

--- client/src/components/atoms/Loading/Loading.tsx
+++ client/src/components/atoms/Loading/Loading.tsx
@@ -3,98 +3,133 @@ import styled, { keyframes } from 'styled-components';
 
 import color from '@/theme/colors';
 
-const drift = keyframes`
-  from {
-    transform: rotate(0deg);
-    opacity: 1;
-  }
-  to {
-    transform: rotate(360deg);
-    opacity: 0;
-    top: -200px;
-    left: 0;
-    width: 1500px;
-    height: 1500px;
-  }
-`;
-
-const Wave = styled.div`
-  position: absolute;
-  top: 20%;
-  left: 48%;
-  border: 5px solid #3781aa;
-  width: 150px;
-  height: 150px;
-  border-radius: 45%;
-  transform-origin: 50% 48%;
-  animation: ${drift} 3s infinite linear;
-`;
-
-const Wave1 = styled(Wave)`
-  border: 5px solid 4s ${color.BLUE};
-`;
-
-const Wave2 = styled(Wave)`
-  border: 6px solid ${color.LIGHT_PURPLE};
-  animation: ${drift} 5s infinite linear;
-`;
-
-const Wave3 = styled(Wave)`
-  border: 7px solid ${color.PALE_PURPLE};
-  animation: ${drift} 7s infinite linear;
-`;
-
-const Wave4 = styled(Wave)`
-  border: 3px solid ${color.LIGHT_GREEN};
-  animation: ${drift} 6s infinite linear;
-`;
-
-const StyledDiv = styled.div`
-  display: block;
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${color.DARK_GRAY};
-  z-index: 11;
-  opacity: 0.5;
-`;
-
-const grow = keyframes`
-  0% {
-    transform: scale(1);
-    opacity: 1;
-  }
-  50% {
-    transform: scale(1.2) skew(10deg);
-    opacity: 0.5;
-  }
-  100% {
-    transform: scale(1);
-    opacity: 1;
-  }
-`;
-
-const StyledP = styled.p`
-  margin: 0;
-  font-size: 1.5rem;
-  position: absolute;
-  top: 30%;
-  left: 45%;
-  animation: ${grow} 4s infinite linear;
-`;
-
 interface Props {
   message: string;
 }
 
+const jump = keyframes`
+   from {
+    top:0px;
+  }
+  to {
+    top:-70px;
+  } 
+`;
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${color.BLACK};
+  z-index: 10;
+  opacity: 0.8;
+  flex-direction: column;
+  font-size: 2rem;
+  font-family: Sans-Serif;
+  font-weight: bold;
+  color: ${color.PALE_PURPLE};
+  text-shadow: 0px 0px 20px ${color.WHITE};
+`;
+
+const StyledSubDiv = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin-top: -120px;
+  z-index: 11;
+`;
+const One = styled.div`
+  background: ${color.DARK_PURPLE};
+  margin-right: 10px;
+  height: 150px;
+  position: relative;
+  width: 50px;
+  &: before {
+    border-bottom: 35px solid ${color.DARK_PURPLE};
+    border-right: 50px solid transparent;
+    content: '';
+    height: 0;
+    left: 0;
+    position: absolute;
+    top: -35px;
+    width: 0;
+  }
+  animation: ${jump} 0.4s 0s ease-in Infinite Alternate;
+`;
+const Two = styled.div`
+  background: ${color.PURPLE};
+  height: 100px;
+  margin-right: 10px;
+  position: relative;
+  width: 50px;
+  &: before {
+    border-bottom: 35px solid ${color.PURPLE};
+    border-right: 50px solid transparent;
+    content: '';
+    height: 0;
+    left: 0;
+    position: absolute;
+    top: -35px;
+    width: 0;
+  }
+  animation: ${jump} 0.4s 0.1s ease-in Infinite Alternate;
+`;
+const Three = styled.div`
+  background: ${color.PALE_PURPLE};
+  height: 85px;
+  position: relative;
+  width: 50px;
+  animation: ${jump} 0.4s 0.2s ease-in Infinite Alternate;
+`;
+const Four = styled.div`
+  background: ${color.MEDIUM_BLUE};
+  height: 100px;
+  position: relative;
+  margin-left: 10px;
+  width: 50px;
+  &: before {
+    border-bottom: 35px solid ${color.MEDIUM_BLUE};
+    border-left: 50px solid transparent;
+    content: '';
+    height: 0;
+    left: 0;
+    position: absolute;
+    top: -35px;
+    width: 0;
+  }
+  animation: ${jump} 0.4s 0.3s ease-in Infinite Alternate;
+`;
+const Five = styled.div`
+  background: ${color.ROYAL_BLUE};
+  height: 150px;
+  position: relative;
+  width: 50px;
+  margin-left: 10px;
+  &: before {
+    border-bottom: 35px solid ${color.ROYAL_BLUE};
+    border-left: 50px solid transparent;
+    content: '';
+    height: 0;
+    left: 0;
+    position: absolute;
+    top: -35px;
+    width: 0;
+  }
+  animation: ${jump} 0.4s 0.4s ease-in Infinite Alternate;
+`;
+
 const Loading: React.FC<Props> = ({ message }) => {
   return (
     <StyledDiv>
-      <StyledP>{message}</StyledP>
-      <Wave1 />
-      <Wave2 />
-      <Wave3 />
-      <Wave4 />
+      <StyledSubDiv>
+        <One />
+        <Two />
+        <Three />
+        <Four />
+        <Five />
+      </StyledSubDiv>
+      {message}
     </StyledDiv>
   );
 };
