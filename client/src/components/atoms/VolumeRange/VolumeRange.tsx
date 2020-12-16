e7975d59c84d6504712ea8d6841ce449df8afb51
권영언 <kyu9341@naver.com>
2020-12-16 17:48:24 +0900

    [FE]: [FIX] 사용하지 않는 import 제거

--- client/src/components/atoms/VolumeRange/VolumeRange.tsx
+++ client/src/components/atoms/VolumeRange/VolumeRange.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
